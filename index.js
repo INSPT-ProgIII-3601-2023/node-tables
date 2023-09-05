63192fcdfe935c55b7a66bcc7e97c3394ef6e99f
jonatan <jonatandeabreu95@gmail.com>
2023-09-05 11:18:03 -0300

    agregar funcion ver tabla y require en index

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
 #!/usr/bin/env node
 const yargs = require('yargs/yargs')
-const { hideBin } = require('yargs/helpers')
+const { hideBin } = require('yargs/helpers');
 const argv = yargs(hideBin(process.argv)).argv
+
+const {get_tabla} = require("./table-manager/manager");
+let {n, max, ver} = argv;
+console.log(get_tabla(n,max));
+
+
+/*
+Esto era antes (es5)
+let n = argv.n;
+let max = argv.max;
+let ver = argv.ver;
+*/
+
+/*es6 (forma moderna)*/ 
+
+
+
+
---
+++ table-manager/manager.js
@@ -0,0 +1,17 @@
+const get_tabla = (n, max = 10) => {
+    /*es5 no es moderna
+    if (!max){
+        max = 10;
+    }
+    */
+    if (!n) {
+        throw "no hay n";
+    };
+    return iota(max).map(x => `${n} x ${x} = ${n * x}`);
+}
+
+const iota = (max = 10) => Array.from(Array(max).keys()).map(x => x + 1);
+
+module.exports = {
+    get_tabla /*esto significa get_tabla:get_tabla*/ 
+};
